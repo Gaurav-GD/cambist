78d37bd45c334881f20228a94408e6bde5209fdd
root <root@webwerks1.(none)>
2014-09-19 17:03:36 +0530

    Logo Changed

--- app/Config/bootstrap.php
+++ app/Config/bootstrap.php
@@ -97,11 +97,11 @@ Configure::write('Dispatcher.filters', array(
  * Configures default file logging options
  */
 App::uses('CakeLog', 'Log');
-CakeLog::config('debug', array(
-	'engine' => 'File',
-	'types' => array('notice', 'info', 'debug'),
-	'file' => 'debug',
-));
+// CakeLog::config('debug', array(
+// 	'engine' => 'File',
+// 	'types' => array('notice', 'info', 'debug'),
+// 	'file' => 'debug',
+// ));
 CakeLog::config('error', array(
 	'engine' => 'File',
 	'types' => array('warning', 'error', 'critical', 'alert', 'emergency'),
